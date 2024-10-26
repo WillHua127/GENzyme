97f25d59b68d142418dd3f417ea436472858445b
WillHua127 <williamhuachenqing@hotmail.com>
2024-10-26 00:03:34 -0400

    Update utils.py

--- data/utils.py
+++ data/utils.py
@@ -65,12 +65,14 @@ def create_full_prot(
     atom37_mask: np.ndarray,
     aatype=None,
     b_factors=None,
+    residue_index=None,
 ):
     assert atom37.ndim == 3
     assert atom37.shape[-1] == 3
     assert atom37.shape[-2] == 37
     n = atom37.shape[0]
-    residue_index = np.arange(n)+1
+    if residue_index is None:
+        residue_index = np.arange(n)
     chain_index = np.zeros(n)
     if b_factors is None:
         b_factors = np.zeros([n, 37])
@@ -83,8 +85,7 @@ def create_full_prot(
         residue_index=residue_index,
         chain_index=chain_index,
         b_factors=b_factors,
-    )
-    
+    )    
 
 def write_pkl(
         save_path: str, pkl_data: Any, create_dir: bool = False, use_torch=False):
